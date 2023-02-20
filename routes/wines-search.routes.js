4cf3e4660353ab571e9d587776778c4a41e7a3e7
Joao Raposo <raposo2281@gmail.com>
2023-02-20 12:12:55 +0000

    changes wine search

--- routes/wines-search.routes.js
+++ routes/wines-search.routes.js
@@ -12,10 +12,20 @@ router.post('/search', async (req, res, next) => {
 
     try {
         let loggedIn = req.session.currentUser;
-        const {searchWine} = req.body;
+        const {searchWine, searchWinery} = req.body;
         console.log(searchWine)
+        
 
-        const wine = await Wine.find({'wine': searchWine});
+
+        let wine 
+        if(searchWine) {
+            wine = await Wine.find({'wine': searchWine});
+            
+        }else{
+            wine = await Wine.find({'winery': searchWinery});
+
+        }
+        
         console.log(wine);
         res.render('wines/result', {wine, loggedIn});
     } catch (error) {
